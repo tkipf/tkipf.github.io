7cf0bf3dc2ffd2ea113eeab15d9b60527770df93
tkipf <thomas.kipf@gmail.com>
2016-09-30 00:01:39 +0200

    rel-links

--- js/init.js
+++ js/init.js
@@ -10,36 +10,36 @@
 		reset: 'full',
 		breakpoints: {
 			global: {
-				href: 'http://tkipf.github.io/css/style.css',
+				href: '/css/style.css',
 				containers: 1400,
 				grid: { gutters: ['2em', 0] }
 			},
 			xlarge: {
 				media: '(max-width: 1680px)',
-				href: 'http://tkipf.github.io/css/style-xlarge.css',
+				href: '/css/style-xlarge.css',
 				containers: 1200
 			},
 			large: {
 				media: '(max-width: 1280px)',
-				href: 'http://tkipf.github.io/css/style-large.css',
+				href: '/css/style-large.css',
 				containers: 960,
 				grid: { gutters: ['1.5em', 0] },
 				viewport: { scalable: false }
 			},
 			medium: {
 				media: '(max-width: 980px)',
-				href: 'http://tkipf.github.io/css/style-medium.css',
+				href: '/css/style-medium.css',
 				containers: '90%!'
 			},
 			small: {
 				media: '(max-width: 736px)',
-				href: 'http://tkipf.github.io/css/style-small.css',
+				href: '/css/style-small.css',
 				containers: '90%!',
 				grid: { gutters: ['1.25em', 0] }
 			},
 			xsmall: {
 				media: '(max-width: 480px)',
-				href: 'http://tkipf.github.io/css/style-xsmall.css'
+				href: '/css/style-xsmall.css'
 			}
 		},
 		plugins: {
